6bd9a98f0675900888514f1d11b60e481e81132a
EverDourado <evertondmcf@hotmail.com>
2023-12-11 19:03:43 -0300

    Import styled-components: button style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import logo from '../../images/LogoAM.png'
-import "./Navbar.css"
+//import "./Navbar.css";
+import styled from "styled-components";
 
 export function Navbar() {
     return (
@@ -11,8 +12,28 @@ export function Navbar() {
                 </div>
                 <img src={logo} alt="Logo Aúncios de Imóveis" />
 
-                <button>ENTRAR</button>
+                <Button>ENTRAR</Button>
             </nav>
         </>
     )
 }
+
+const Button = styled.button`
+    background-color: #0bade3;
+    border: none;
+    outline: none;
+    font-size: 0.9rem;
+    padding: 0.4rem 1rem;
+    color: #fff;
+    transition: all 0.4s ease-in-out;
+    cursor: pointer;
+    border-radius: 0.3rem;
+    font-family: Roboto, arial;
+    /* width: 5%; */
+    font-weight: 500;
+    letter-spacing: 0.1rem;
+
+    &:hover {
+    background-color: #0a86af;
+}
+`
